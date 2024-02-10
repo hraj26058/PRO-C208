4134297666a894bc37215014b8426666324177fb
Harsh Raj <81627508+hraj26058@users.noreply.github.com>
2024-02-10 14:28:04 +0530

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,49 @@
+import socket
+from  threading import Thread
+import os
+
+IP_ADDRESS = '127.0.0.1'
+PORT = 8050
+SERVER = None
+BUFFER_SIZE = 4096
+clients = {}
+
+#Creating shared_files directory on server
+is_dir_exists = os.path.isdir('shared_files')
+print(is_dir_exists)
+if(not is_dir_exists):
+    os.makedirs('shared_files')
+
+def acceptConnections():
+    global SERVER
+    global clients
+
+    while True:
+        client, addr = SERVER.accept()
+        print(f"Connection established with {client} : {addr}")
+        
+def setup():
+    print("\n\t\t\t\t\t\tIP MESSENGER\n")
+
+    # Getting global values
+    global PORT
+    global IP_ADDRESS
+    global SERVER
+
+
+    SERVER  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+
+    # Listening incomming connections
+    SERVER.listen(100)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...")
+    print("\n")
+
+    acceptConnections()
+
+
+setup_thread = Thread(target=setup)
+setup_thread.start()
+
+
